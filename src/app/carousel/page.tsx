af6b8b5a89631f6cbde50bf21aaaac7d91410703
Peter Madrid <petermadrid0421@gmail.com>
2025-10-21 05:10:49 +0800

    updated responsiveness and design and exported anim to global

--- src/app/carousel/page.tsx
+++ src/app/carousel/page.tsx
@@ -1,10 +1,14 @@
-'use client';
-
 import Image from 'next/image';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+    weight: ['400', '700'], // Add the weights you need
+    subsets: ['latin'],
+});
 
 export default function Carousel() {
     return (
-        <div className="relative mt-20 w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[664px] overflow-hidden">
+        <div className="relative mt-20 w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[90vh] overflow-hidden">
             <div className="absolute inset-0 flex animate-scroll will-change-transform">
                 <div className="flex-shrink-0 relative h-full w-[200vw] sm:w-[150vw]">
                     <Image
@@ -33,28 +37,14 @@ export default function Carousel() {
             {/* Black opacity overlay */}
             <div className="absolute inset-0 bg-black/50 pointer-events-none" />
             {/* Centered text */}
-            <div className="absolute inset-0 flex justify-center items-center pointer-events-none">
-                <p className="font-sans italic font-light tracking-tight leading-tight text-white text-xl sm:text-3xl md:text-4xl lg:text-5xl">
-                    We make it more than just special!
+            <div className="flex flex-col absolute p-8 md:p-12 lg:p-16 xl:p-32 text-white inset-0 flex justify-center items-start pointer-events-none">
+                <p className={`${poppins.className} font-bold tracking-wider leading-tighter text-5xl sm:text-6xl md:text-7xl lg:text-8xl`}>
+                    WE MAKE <br></br> IT MORE <br></br> THAN JUST <br></br> SPECIAL
                 </p>
+                <div className={`${poppins.className} flex gap-2 mt-4 p-4 lg:text-xl lg:p-6 tracking-widest border`}>
+                    <p>MESSAGE US</p>
+                </div>
             </div>
-
-
-            <style dangerouslySetInnerHTML={{
-                __html: `
-                    @keyframes scroll {
-                        0% { transform: translate3d(0, 0, 0); }
-                        100% { transform: translate3d(-100vw, 0, 0); }
-                    }
-                    
-                    .animate-scroll {
-                        animation: scroll 120s linear infinite;
-                    }
-                    
-                    .will-change-transform {
-                        will-change: transform;
-                    }
-                `}} />
         </div>
     )
 }
