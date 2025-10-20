542d0bd0d1b304ce4cc850330017189c40fd02d1
Peter Madrid <petermadrid0421@gmail.com>
2025-10-21 07:18:02 +0800

    Added 3 content offers, and initial images

---
+++ src/app/offer/cards/Card.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface CardProps {
+    image: string;
+    title: string;
+    description: string;
+    href: string;
+}
+
+export default function Card({ image, title, description, href }: CardProps) {
+    return (
+        <Link href={href} className="group">
+            <div className="flex flex-col text-white bg-neutral-900 rounded-tr-4xl rounded-bl-4xl p-4 w-[300px] sm:justify-self-end cursor-pointer transition-transform duration-300 ease-out group-hover:-translate-y-2">
+                <div className="relative bg-white w-full h-[300px] rounded-tr-2xl rounded-bl-2xl overflow-hidden">
+                    <Image
+                        src={image}
+                        alt={title}
+                        fill
+                        className="object-cover transition-transform duration-300"
+                    />
+                </div>
+
+                <div className="flex mt-4 justify-center font-bold text-lg">
+                    {title}
+                </div>
+
+                <div className="flex mt-4 text-center">
+                    {description}
+                </div>
+            </div>
+        </Link>
+    );
+}
