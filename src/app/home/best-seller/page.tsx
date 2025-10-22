57cfed394aed7cad4074b13b62c801d870000f23
Peter Madrid <petermadrid0421@gmail.com>
2025-10-22 14:48:41 +0800

    best-seller

---
+++ src/app/home/best-seller/page.tsx
@@ -0,0 +1,33 @@
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+
+const poppins = Poppins({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+
+export default function BestSeller() {
+    return (
+        <div className={`${poppins.className} mt-12 p-8 text-white text-xs sm:text-sm w-full bg-neutral-900`}>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-5xl mx-auto">
+                <div className="relative min-h-120 overflow-hidden">
+                    <Image
+                        src="/landing-page/best-seller.avif"
+                        fill
+                        alt="Best seller"
+                        className="object-cover ambient"
+                    />
+                </div>
+                <div className="flex flex-col justify-between sm:justify-normal gap-6 p-6 min-h-80">
+                    <h2 className="text-4xl font-bold">This Month's Best Seller</h2>
+                    <p className="text-lg text-neutral-300 leading-relaxed">
+                        Our Angel's Bouquet has been stealing hearts all month! Loved for its soft tones and timeless charm — no wonder it's everyone's favorite pick.
+                    </p>
+                    <a className="border border-white hover:bg-white hover:text-black transition-colors cursor-pointer py-6 px-12 text-[1.1rem] w-fit">
+                        Learn More
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
 import Nav from "./Nav";
 import ScrollHeader from "./ScrollHeader";
+import Image from "next/image";
 
 export default function Header() {
     return (
         <ScrollHeader>
-            <div className="mx-auto flex max-w-6xl items-center justify-between p-8">
-                <h1 className="text-lg font-semibold tracking-tight">Kraft & Serenade</h1>
+            <div className="mx-auto flex max-w-6xl items-center justify-between px-8 py-2">
+                <div className="relative h-20 w-55">
+                    <Image
+                        src="/logo-ks.png"
+                        alt="logo"
+                        fill
+                        className="object-cover"
+                    />
+                </div>
                 <Nav />
             </div>
         </ScrollHeader>
