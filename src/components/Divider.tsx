d802d8b34bbbf64c617e4f0988451ea30196820d
Peter Madrid <petermadrid0421@gmail.com>
2025-10-27 07:32:22 +0800

    removed repeated imports

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,14 +1,6 @@
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({
-    weight: ['400', '700'],
-    subsets: ['latin'],
-});
-
-
 export default function Divider() {
     return (
-        <div className={`${poppins.className} flex flex-col text-xs sm:text-sm justify-center items-center w-full h-[8vh] sm:h-[9vh] md:h-[14vh] bg-neutral-900`}>
+        <div className={`flex flex-col text-xs sm:text-sm justify-center items-center w-full h-[8vh] sm:h-[9vh] md:h-[14vh] bg-neutral-900`}>
             <p className="text-neutral-300 font-semibold">“True beauty never fades — it blossoms.”</p>
             <p className="text-neutral-400">-Unkown</p>
         </div>
