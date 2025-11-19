1f825aaac4cf3404038c727b30ae1d2504cf0794
Peter Madrid <petermadrid0421@gmail.com>
2025-11-19 18:57:20 +0800

    bugfix for deployment error

--- src/app/home/add-ons/Addons.tsx
+++ src/app/home/add-ons/Addons.tsx
@@ -6,7 +6,7 @@ export default function Addons() {
     return (
         <div className="mt-12">
             <div className="max-w-6xl mx-auto px-4">
-                <p className="text-4xl text-neutral-800">Addons</p>
+                <p className="text-4xl text-neutral-800">{`Add-ons`}</p>
                 <p className="text-neutral-600 mt-2">
                     Thoughtfully crafted finishing touches<br />
                     our add-ons bring a refined charm that elevates every bouquet.
