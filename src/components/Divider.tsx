e7101eff689c06528b2363d167027e8505249df0
Peter Madrid <petermadrid0421@gmail.com>
2026-06-11 00:51:07 +0800

    fix build error

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -16,7 +16,9 @@ export default function Divider() {
 
     return (
         <div className="flex flex-col text-xs sm:text-sm justify-center items-center w-full h-[8vh] sm:h-[9vh] md:h-[14vh] bg-neutral-900">
-            <p className="text-neutral-300 font-semibold">"{quote.quote}"</p>
+            <p className="text-neutral-300 font-semibold">
+                {'"'}{quote.quote}{'"'}
+            </p>
             <p className="text-neutral-400">— {quote.author || "Unknown"}</p>
         </div>
     );
